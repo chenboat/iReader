071e43500cdf2017cb79450deeb4b34ea5b26ecb
chenboat <chenboat@gmail.com>
2014-09-28 14:35:57 -0700

    Add pics for top ranked ariticles

--- src/main/java/com/intelliReader/jetty/HTMLUtil.java
+++ src/main/java/com/intelliReader/jetty/HTMLUtil.java
@@ -1,8 +1,18 @@
 package com.intelliReader.jetty;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 import org.eclipse.jetty.server.Request;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created with IntelliJ IDEA.
@@ -11,6 +21,9 @@ import java.io.IOException;
  * Time: 3:39 PM
  */
 public class HTMLUtil {
+    private static final Pattern p = Pattern.compile(
+            "http://graphics..nytimes.com/[^\"]*thumbLarge.jpg|http://static.*.nyt.com/.*.jpg"); // the regex to match the picture url
+
     public static void setHTMLPagePrelude(Request baseRequest,
                                           HttpServletResponse response) throws IOException {
         response.setContentType("text/html;charset=utf-8");
@@ -21,5 +34,45 @@ public class HTMLUtil {
 
     public static void setHTMLPageEpilogue(HttpServletResponse response) throws IOException {
         response.getWriter().println("</body>\n" + "</html>");
+    }
+
+    /**
+     *
+     * @param url a nytimes link
+     * @return a url for the picture in the link
+     */
+    public static String getPicURLFromNYTimesLink(String url) throws IOException{
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        try {
+            HttpGet httpget = new HttpGet(url);
+            // Create a custom response handler
+            ResponseHandler<String> responseHandler = new ResponseHandler<String>() {
+
+                public String handleResponse(
+                        final HttpResponse response) throws ClientProtocolException, IOException {
+                    int status = response.getStatusLine().getStatusCode();
+                    if (status >= 200 && status < 300) {
+                        HttpEntity entity = response.getEntity();
+                        return entity != null ? EntityUtils.toString(entity) : null;
+                    } else {
+                        throw new ClientProtocolException("Unexpected response status: " + status);
+                    }
+                }
+
+            };
+
+            String str = httpclient.execute(httpget, responseHandler);
+            Matcher m = p.matcher(str);
+
+            while(m.find()) return m.group();
+
+        } catch (IOException e) {
+            return null; // the best effect delivery
+
+        } finally
+        {
+            httpclient.close();
+        }
+        return null;
     }
 }
---
+++ src/test/java/jetty/TestHTMLUtil.java
@@ -0,0 +1,34 @@
+package jetty;
+
+import com.intelliReader.jetty.HTMLUtil;
+import junit.framework.TestCase;
+
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * User: ting
+ * Date: 9/27/14
+ * Time: 11:36 AM
+ */
+public class TestHTMLUtil extends TestCase {
+
+    public void testURLReader() throws IOException {
+        String[] str = {
+                    "http://rss.nytimes.com/c/34625/f/640305/s/3ed60b43/sc/39/l/0L0Snytimes0N0C20A140C0A90C260Cworld0Cafrica0Csouth0Esudan0Echina0Eto0Esend0Etroops0Efor0Eun0Emission0Bhtml0Dpartner0Frss0Gemc0Frss/story01.htm",
+                    "http://rss.nytimes.com/c/34625/f/640347/s/3ec10cb6/sc/29/l/0Ldotearth0Bblogs0Bnytimes0N0C20A140C0A90C230Cnew0Eco20Eemissions0Ereport0Eshows0Echinas0Ecentral0Erole0Ein0Eshaping0Eworlds0Eclimate0Epath0C0Dpartner0Frss0Gemc0Frss/story01.htm",
+                    "http://rss.nytimes.com/c/34625/f/640310/s/3eb443ed/sc/39/l/0L0Snytimes0N0C20A140C0A90C230Cworld0Casia0Chong0Ekong0Estudents0Elead0Edemocracy0Efight0Ewith0Eclass0Eboycott0Bhtml0Dpartner0Frss0Gemc0Frss/story01.htm",
+                    "http://rss.nytimes.com/c/34625/f/640310/s/3eddc8cf/sc/7/l/0L0Snytimes0N0C20A140C0A90C270Cworld0Casia0Cindia0Echina0Eladakh0Edispute0Bhtml0Dpartner0Frss0Gemc0Frss/story01.htm",
+                    "http://rss.nytimes.com/c/34625/f/640329/s/3ed568f2/sc/22/l/0L0Snytimes0N0C20A140C0A90C260Cbusiness0Cinternational0Cin0Echina0Esteel0Econsumption0Edrops0Eas0Eeconomy0Eslows0Bhtml0Dpartner0Frss0Gemc0Frss/story01.htm",
+                    "http://rss.nytimes.com/c/34625/f/667200/s/3ee4a63d/sc/11/l/0L0Snytimes0N0C20A140C0A90C280Cworld0Cafrica0Cliberia0Ehealth0Echief0Eis0Eunder0Equarantine0Bhtml0Dpartner0Frss0Gemc0Frss/story01.htm",
+                    "http://rss.nytimes.com/c/34625/f/640350/s/3ee93a60/sc/13/l/0L0Snytimes0N0C20A140C0A90C290Csports0Cgolf0Cryder0Ecup0E20A140Eeuropeans0Emake0Eeasy0Ework0Eof0Eus0Efor0Ethird0Estraight0Evictory0Bhtml0Dpartner0Frss0Gemc0Frss/story01.htm"
+        };
+
+        for(String s:str){
+            System.out.println(HTMLUtil.getPicURLFromNYTimesLink(s));
+        }
+
+    }
+}
