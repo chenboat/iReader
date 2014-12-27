139b3a974f646628b165197a442d31249e1eb707
chenboat <chenboat@gmail.com>
2014-12-26 19:03:18 -0800

    reduced the number of page to craw

--- src/main/java/com/intelliReader/jetty/JettyServer.java
+++ src/main/java/com/intelliReader/jetty/JettyServer.java
@@ -126,7 +126,7 @@ public class JettyServer extends ServletContextHandler {
         }
         List<KeywordBasedFeedRelevanceModel.ScoredFeedMessage> rankedList =
                 model.rankFeeds(feedMsgs, Calendar.getInstance().getTime());
-        int topK = 300; // Add pic only to the topK feed msg
+        int topK = 30; // Add pic only to the topK feed msg
         int cnt = 0;
         for(KeywordBasedFeedRelevanceModel.ScoredFeedMessage msg: rankedList){
             FeedMessage message = msg.getMsg();
