7a2a2efae79ed386460ad9ba9f0ad1be99f8a7f7
chenboat <chenboat@gmail.com>
2016-03-30 22:13:31 -0700

    Allow ? in the regex matching string

--- src/main/java/com/intelliReader/jetty/HTMLUtil.java
+++ src/main/java/com/intelliReader/jetty/HTMLUtil.java
@@ -23,7 +23,7 @@ import java.util.regex.Pattern;
 public class HTMLUtil {
     public static final String ACCOUNT_DELIMITER = ":";
     private static final Pattern p = Pattern.compile(
-            "http://graphics..nytimes.com/[^\"]*thumbLarge.jpg|http://static[^\"]*.nyt.com/[^\"]*.jpg"); // the regex to match the picture url
+            "https?://graphics..nytimes.com/[^\"]*thumbLarge.jpg|https?://static[^\"]*.nyt.com/[^\"]*.jpg"); // the regex to match the picture url
 
     public static void setHTMLPagePrelude(Request baseRequest,
                                           HttpServletResponse response) throws IOException {
